90c9165d7a9d4afa3c5bbe6a17cb38bc569b3aec
EduardoAzuul <0260522@up.edu.mx>
2024-10-30 14:39:55 -0600

    lista de valores

--- BTS.py
+++ BTS.py
@@ -10,6 +10,7 @@ for v in l:
 
 lista_in_orden = abb.inorden(abb.raiz)
 lista_niveles =[]
+valores_nodos=[]
 cantidad_nodo= len(lista_in_orden)
 
 
@@ -18,11 +19,14 @@ for v in lista_in_orden:
 
     nodo_aux= v
     lista_niveles.append(profundidad(nodo_aux))
+    valores_nodos.append( nodo_aux.valor)
 
 maximo_nivel = max(lista_niveles)+1
 numero_nodos = len(lista_in_orden)
 
+
 print(lista_in_orden)
+print(valores_nodos)
 print(lista_niveles)
 print(f"Tenemos {maximo_nivel} niveles")
 print(f"Tenemos {numero_nodos} nodos")
