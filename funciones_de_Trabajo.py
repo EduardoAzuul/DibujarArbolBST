23abc3f95ad83af68fd9e76d17771ad3f8d458cd
EduardoAzuul <0260522@up.edu.mx>
2024-11-04 16:27:59 -0600

    Correcion de errores en eliminar

--- funciones_de_Trabajo.py
+++ funciones_de_Trabajo.py
@@ -16,12 +16,10 @@ class Nodo:
   
 
   
-def mayor(self, nodo):
+def mayor(nodo):
     while nodo.der:  # Recorre a la derecha mientras haya nodos
         nodo = nodo.der
     return nodo  
-
-
 
 
 def profundidad(nodo): #Si se puede evitar la recursividad, mejor
@@ -135,37 +133,57 @@ class BST:
             return None
           actual = actual.der
 
+  def eliminar(self, valor):  # Añadido método eliminar al BST
+          nodo = self.buscar(valor)
+          if nodo:
+              self.eliminar_nodo(nodo)
+          return None
+  
   def eliminar_nodo(self,nodo):
 
     # Caso 1: Nodo hoja
     if not nodo.izq and not nodo.der:
-      if nodo == self.raiz:
+      if nodo == nodo.padre:
           self.raiz = None
       else:
           padre = nodo.padre
-          if  padre.izq == nodo:
-            padre.izq = None
+          if padre.izq == nodo:
+              padre.izq = None
           else:
               padre.der = None
           nodo.padre = None
-          return nodo
-    
-    if(nodo.izq == None and nodo.der == None):
-        limpiar(nodo)
 
    # Caso 2: Nodo con un hijo
-    if not nodo.izq:  # Tiene hijo derecho
-       self._reemplazar_nodo(nodo, menor(nodo.der))
-       return nodo
-    elif not nodo.der:  # Tiene hijo izquierdo
-       hijo = nodo.izq
-       self._reemplazar_nodo(nodo, mayor(nodo.izq))
-       return nodo
+    elif nodo.izq and not nodo.der:
+      if nodo == self.raiz:
+          self.raiz = nodo.izq
+          nodo.izq.padre = None
+      else:
+          padre = nodo.padre
+          if padre.izq == nodo:
+              padre.izq = nodo.izq
+          else:
+              padre.der = nodo.izq
+          nodo.izq.padre = padre
     
+    elif nodo.der and not nodo.izq:
+      if nodo == self.raiz:
+          self.raiz = nodo.der
+          nodo.der.padre = None
+      else:
+          padre = nodo.padre
+          if padre.izq == nodo:
+              padre.izq = nodo.der
+          else:
+              padre.der = nodo.der
+          nodo.der.padre = padre
+
     # Caso 3: Nodo con dos hijos
-    sucesor = self.mayor(nodo.izq)  # O self.mayor(nodo.izq)
-    nodo.valor = sucesor.valor
-    return self.eliminar_nodo(sucesor)
+    else:
+      sucesor = self.mayor(nodo.izq)
+      nodo.valor = sucesor.valor
+      self.eliminar_nodo(sucesor)
+      print(sucesor)
 
   def inorden(self, nodo):
       l=[]
@@ -174,19 +192,32 @@ class BST:
       if nodo.der : l.extend(self.inorden(nodo.der))
       return l
 
-  def _reemplazar_nodo(self, nodo, nuevo_nodo):
-          if nodo == self.raiz:
-              self.raiz = nuevo_nodo
-          else:
-              padre = nodo.padre
-              if padre.izq == nodo:
-                  padre.izq = nuevo_nodo
-              else:
-                  padre.der = nuevo_nodo
-          if nuevo_nodo:
-              nuevo_nodo.padre = nodo.padre
-          
-          limpiar(nodo)
+def _reemplazar_nodo(self, nodo, nuevo_nodo):
+    
+    if nodo.padre == None:  # Es la raíz
+        self.raiz = nuevo_nodo
+        nuevo_nodo.padre = None
+    else:
+        padre = nodo.padre
+        if padre.izq == nodo:
+            padre.izq = nuevo_nodo
+        else:
+            padre.der = nuevo_nodo
+        if nuevo_nodo:
+            nuevo_nodo.padre = padre
+    
+    if nodo.der:
+       nuevo_nodo.der=nodo.der
+       nodo.der.padre=nuevo_nodo
+
+    if nodo.izq:
+       nuevo_nodo.der=nodo.izq
+       nodo.izq.padre=nuevo_nodo
+    
+    limpiar(nodo)  # Limpiamos el nodo original, no el nuevo
+    nodo.valor=nuevo_nodo.valor
+
+    limpiar(nuevo_nodo)
 
 def limpiar(nodo):
     nodo.valor= None
@@ -194,3 +225,10 @@ def limpiar(nodo):
     nodo.izq=None
     nodo.padre=None
 
+    if not nodo.padre == None:  ##Si no es su raiz
+          padre = nodo.padre
+          if  padre.izq == nodo:
+            padre.izq = None
+          else:
+              padre.der = None
+          
